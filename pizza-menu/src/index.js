d4d31fbd82b150dc2258a919380ab8312e4aeea4
Stephan Raab <sarcartist@gmail.com>
2024-12-05 16:57:19 -0500

    more styling

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -44,9 +44,11 @@ function Pizza(props) {
     return (
         <div className="pizza">
             <img src={props.photoName} alt={props.name}/>
-            <h3>{props.name}</h3>
-            <p>{props.ingredients}</p>
-            <p>{props.price}</p>
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
         </div>
     )
 }
