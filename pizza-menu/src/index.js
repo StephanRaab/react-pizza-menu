a1aea672aec958e3fbbbe513e0e38564d8391bdd
Stephan Raab <sarcartist@gmail.com>
2024-12-11 18:48:44 -0500

    update prop passing and use ul>li

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,71 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import pizzaData from './data/pizzaData.json';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import pizzaData from "./data/pizzaData.json";
 
 function App() {
-    return (
-        <div className={"container"}>
-            <Header />
-            <Menu/>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className={"container"}>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 function Header() {
-    return (
-        <header className="header">
-            <h1 className={"header"}>React Pizza Co.</h1>
-        </header>
-    )
+  return (
+    <header className="header">
+      <h1 className={"header"}>React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
-    return (
-        <section>
-            <main className="menu">
-                <h2>Our Menu</h2>
-                {pizzaData.pizzas.map((pizza, i) => {
-                    return (
-                        <Pizza key={i}
-                               name={pizza.name}
-                               photoName={pizza.photoName}
-                               ingredients={pizza.ingredients}
-                               price={pizza.price} />
-                        )
-                })}
-            </main>
-        </section>
-    )
+  return (
+    <section>
+      <main className="menu">
+        <h2>Our Menu</h2>
+        <ul className="pizzas">
+          {pizzaData.pizzas.map((pizza, i) => {
+            return <Pizza pizza={pizza} key={i + pizza.name} />;
+          })}
+        </ul>
+      </main>
+    </section>
+  );
 }
 
 function Pizza(props) {
-    return (
-        <div className="pizza">
-            <img src={props.photoName} alt={props.name}/>
-            <div>
-                <h3>{props.name}</h3>
-                <p>{props.ingredients}</p>
-                <span>{props.price}</span>
-            </div>
-        </div>
-    )
+  return (
+    <li className="pizza">
+      <img src={props.pizza.photoName} alt={props.pizza.name} />
+      <div>
+        <h3>{props.pizza.name}</h3>
+        <p>{props.pizza.ingredients}</p>
+        <span>{props.pizza.price}</span>
+      </div>
+    </li>
+  );
 }
 
 function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closedHour = 22;
-    const isOpen = hour >= openHour && hour < closedHour;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closedHour = 22;
+  const isOpen = hour >= openHour && hour < closedHour;
 
-    return (
-        <footer className={"footer"}>
-            {new Date().toLocaleTimeString()}, we're currently open.
-        </footer>
-    )
+  return (
+    <footer className={"footer"}>
+      {new Date().toLocaleTimeString()}, we're currently open.
+    </footer>
+  );
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode><App/></React.StrictMode>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
