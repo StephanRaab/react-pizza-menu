52f69419129d64dc13145f286f1e16b75d4fbfc8
Stephan Raab <sarcartist@gmail.com>
2024-12-11 19:59:51 -0500

    prop destructuring to clean up the code

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -43,16 +43,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizza.soldOut) return null; // this is just temporary for now
+function Pizza({ pizza }) {
+  if (pizza.soldOut) return null; // this is just temporary for now
 
   return (
     <li className="pizza">
-      <img src={props.pizza.photoName} alt={props.pizza.name} />
+      <img src={pizza.photoName} alt={pizza.name} />
       <div>
-        <h3>{props.pizza.name}</h3>
-        <p>{props.pizza.ingredients}</p>
-        <span>{props.pizza.price}</span>
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>{pizza.price}</span>
       </div>
     </li>
   );
